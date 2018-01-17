efd1ec8973eb244aad25ee663e42600cbfddf619
ZIV LEVY <zivilevy@gmail.com>
2018-01-17 17:36:37 +0200

    JSON Tree stage 4

---
+++ src/app/views/organization/org-tree-edit-free-docs/org-tree-edit-free-docs.component.ts
@@ -0,0 +1,74 @@
+import {Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Subject} from "rxjs/Subject";
+import {IActionMapping, ITreeOptions, TREE_ACTIONS} from "angular-tree-component";
+import {OrgService} from "../org.service";
+
+@Component({
+  selector: 'sk-org-tree-edit-free-docs',
+  templateUrl: './org-tree-edit-free-docs.component.html',
+  styleUrls: ['./org-tree-edit-free-docs.component.scss']
+})
+export class OrgTreeEditFreeDocsComponent implements OnInit, OnDestroy {
+
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  @Input() isRTL: boolean;
+  @ViewChild('tree') tree;
+  nodes: Array<any> = [{id: '0', name: 'root', children: []}];
+  tree_options: ITreeOptions;
+
+  constructor(public orgService: OrgService) {
+  }
+
+  ngOnInit() {
+    this.tree_options = {
+      rtl: this.isRTL,
+      displayField: 'name',
+      childrenField: 'children',
+      actionMapping: this.getTreeActionMapping(),
+      allowDrag: (node) => {
+        return true;
+      },
+      allowDrop: (element: any, to: any) => {
+        // console.log(to)
+        return false;
+      }
+    };
+
+    this.orgService.getTreeOrgDocs$()
+      .takeUntil(this.destroy$)
+      .subscribe( docs => {
+        console.log( docs);
+        this.nodes = docs;
+      });
+  }
+
+  private getTreeActionMapping(): IActionMapping {
+    return {
+      keys: {
+        // [KEYS.RIGHT]: null,
+        // [KEYS.LEFT]: null,
+        // [KEYS.DOWN]: null,
+        // [KEYS.UP]: null,
+        // [KEYS.SPACE]: null,
+        // [KEYS.ENTER]: null
+      },
+      mouse: {
+        click: TREE_ACTIONS.TOGGLE_ACTIVE,
+        dblClick: null,
+        contextMenu: null,
+        expanderClick: TREE_ACTIONS.TOGGLE_EXPANDED,
+        checkboxClick: TREE_ACTIONS.TOGGLE_SELECTED,
+        drop: TREE_ACTIONS.MOVE_NODE
+      },
+    };
+  }
+
+
+  ngOnDestroy() {
+    // force unsubscribe
+    this.destroy$.next(true);
+    // Now let's also unsubscribe from the subject itself:
+    this.destroy$.unsubscribe();
+
+  }
+}
---
+++ src/app/views/organization/org-tree-edit-manager/org-tree-edit-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sk-org-tree-edit-manager',
+  templateUrl: './org-tree-edit-manager.component.html',
+  styleUrls: ['./org-tree-edit-manager.component.scss']
+})
+export class OrgTreeEditManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
