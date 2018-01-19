8ecc982e0d49e4dcf0ab7621559b0e38baa004e6
ZIV LEVY <zivilevy@gmail.com>
2018-01-19 10:40:11 +0200

    Added type warning to edit

---
+++ src/app/views/document/document.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { SkItem, SkSection} from '../../model/document';
+
+@Injectable()
+export class DocumentService {
+
+  constructor() { }
+
+
+  SkTreeListFronJSON(docJson: string) {
+    const docObject = JSON.parse(docJson);
+    const docTree = new SkSection().deserialize(docObject);
+    console.log(docTree);
+    const resultArry = [];
+    console.log(this.makeTreeList(docTree, resultArry));
+  }
+
+
+  makeTreeList(docTreeItem: SkSection | SkItem, resultArray) {
+    if (docTreeItem instanceof SkSection) {
+      resultArray.push(docTreeItem);
+      docTreeItem.nodes.forEach(item => {
+        this.makeTreeList(item, resultArray);
+      });
+    } else {
+      resultArray.push(docTreeItem);
+    }
+    return resultArray;
+  }
+}
