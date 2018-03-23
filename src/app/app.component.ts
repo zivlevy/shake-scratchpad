ad14848ce45d779ee3ca4c841a00a25806bc213e
Ran Levy <RanLevy62@gmail.com>
2018-03-23 17:30:56 +0300

    Graphic design - organize material theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
 
 
     rtl = true;
-    content;
+    // content;
     constructor(private translate: TranslateService,
                 private authService: AuthService,
                 private lngService: LanguageService) {
@@ -28,30 +28,31 @@ export class AppComponent implements OnInit {
           // the lang to use, if the lang isn't available, it will use the current loader to get them
           this.translate.use(lng);
           lng === 'he' ? this.rtl = true : this.rtl = false;
-          const bodyRoot: HTMLElement = <HTMLElement> document.getElementsByTagName('body')[0];
-          if (bodyRoot != null) {
-            if (lng === 'he') {
-              bodyRoot.style.fontFamily = 'Rubik';
-            } else {
-              bodyRoot.style.fontFamily = 'Roboto';
-            }
-          }
-          const htmlRoot: HTMLElement = <HTMLElement> document.getElementsByTagName('html')[0];
-          if (htmlRoot != null) {
-            if (lng === 'he') {
-              htmlRoot.style.fontFamily = 'Rubik';
-            } else {
-              htmlRoot.style.fontFamily = 'Roboto';
-            }
-          }
-          const matCard: HTMLElement = <HTMLElement> document.getElementsByTagName('mat-card')[0];
-          if (matCard != null) {
-            if (lng === 'he') {
-              matCard.style.fontFamily = 'Rubik';
-            } else {
-              matCard.style.fontFamily = 'Roboto';
-            }
-          }
+
+          // const bodyRoot: HTMLElement = <HTMLElement> document.getElementsByTagName('body')[0];
+          // if (bodyRoot != null) {
+          //   if (lng === 'he') {
+          //     bodyRoot.style.fontFamily = 'Rubik';
+          //   } else {
+          //     bodyRoot.style.fontFamily = 'Roboto';
+          //   }
+          // }
+          // const htmlRoot: HTMLElement = <HTMLElement> document.getElementsByTagName('html')[0];
+          // if (htmlRoot != null) {
+          //   if (lng === 'he') {
+          //     htmlRoot.style.fontFamily = 'Rubik';
+          //   } else {
+          //     htmlRoot.style.fontFamily = 'Roboto';
+          //   }
+          // }
+          // const matCard: HTMLElement = <HTMLElement> document.getElementsByTagName('mat-card')[0];
+          // if (matCard != null) {
+          //   if (lng === 'he') {
+          //     matCard.style.fontFamily = 'Rubik';
+          //   } else {
+          //     matCard.style.fontFamily = 'Roboto';
+          //   }
+          // }
 
         });
     }
