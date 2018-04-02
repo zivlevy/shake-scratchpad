fa92a7361686cf8929dddd3b842050fe836cf5cf
Ran Levy <RanLevy62@gmail.com>
2018-04-02 19:58:44 +0300

    Deleted obsolete files

--- src/app/views/organization/organization-routing.module.ts
+++ src/app/views/organization/organization-routing.module.ts
@@ -1,12 +1,10 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {OrgHomePageComponent} from './org-home-page/org-home-page.component';
 import {OrgAdminUsersComponent} from './org-admin-users/org-admin-users.component';
 import {OrgAdminOrgComponent} from './org-admin-org/org-admin-org.component';
 import {UserProfileComponent} from '../../shared/user-profile/user-profile.component';
 import {LoginComponent} from '../../shared/login/login.component';
 import {SignupComponent} from '../../shared/signup/signup.component';
-import {OrgSearchDocsComponent} from './org-search-docs/org-search-docs.component';
 import {OrgDocEditComponent} from './org-doc-edit/org-doc-edit.component';
 import {OrgDocViewComponent} from './org-doc-view/org-doc-view.component';
 import {OrgDocReadAcksComponent} from './org-doc-read-acks/org-doc-read-acks.component';
--- src/app/views/organization/organization.module.ts
+++ src/app/views/organization/organization.module.ts
@@ -2,13 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CoreModule} from '../../core/core.module';
 import {OrganizationRoutingModule} from './organization-routing.module';
-import {OrgHomePageComponent} from './org-home-page/org-home-page.component';
 import {OrgGuard} from './guards/org.guard';
 import {OrgService} from './org.service';
 import {TranslateModule} from '@ngx-translate/core';
 import {ReactiveFormsModule , FormsModule} from '@angular/forms';
 import {SharedModule} from '../../shared/shared.module';
-import { OrgNavComponent } from './org-nav/org-nav.component';
 import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 import { OrgAdminUsersComponent } from './org-admin-users/org-admin-users.component';
 import { OrgAdminOrgComponent } from './org-admin-org/org-admin-org.component';
@@ -21,7 +19,6 @@ import {MaterialModule} from '../../material/material.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {OrgMessagesComponent} from './org-messages/org-messages.component';
 import {OrgTreeViewComponent} from './org-tree-view/org-tree-view.component';
-import { OrgSearchDocsComponent } from './org-search-docs/org-search-docs.component';
 import { OrgDocViewComponent } from './org-doc-view/org-doc-view.component';
 import { OrgDocEditComponent } from './org-doc-edit/org-doc-edit.component';
 import { PublishDialogComponent } from './dialogs/publish-dialog/publish-dialog.component';
@@ -57,13 +54,10 @@ import { OrgSearchDocComponent } from './org-search-doc/org-search-doc.component
 
   ],
   declarations: [
-    OrgHomePageComponent,
-    OrgNavComponent,
     OrgAdminUsersComponent,
     OrgAdminOrgComponent,
     OrgMessagesComponent,
     OrgTreeViewComponent,
-    OrgSearchDocsComponent,
     OrgDocViewComponent,
     OrgDocEditComponent,
     PublishDialogComponent,
@@ -79,7 +73,6 @@ import { OrgSearchDocComponent } from './org-search-doc/org-search-doc.component
     OrgSearchDocComponent,
   ],
   exports: [
-    OrgHomePageComponent,
     OrgContainerComponent,
   ],
   entryComponents: [
