186f596dcdca7cf229f52c76a3443de1603b177c
Ran Levy <RanLevy62@gmail.com>
2018-04-17 18:03:46 +0300

    New Doc Section view logic

--- src/app/views/document/task-viewer/task-viewer.component.ts
+++ src/app/views/document/task-viewer/task-viewer.component.ts
@@ -17,10 +17,10 @@ export class TaskViewerComponent implements OnInit, OnChanges {
 
   searchPhrase: string = '';
   isSearch: boolean = false;
-  searchTaskArray: Array<number> = [];
-  currentSearchTask: number = 0;
-  taskList: Array<SkSection>;
-  viewTask: Array<SkSection | SkItem>;
+  searchSectionArray: Array<number> = [];
+  currentSearchSectionIndex: number = 0;
+  sectionList: Array<SkSection>;
+  viewSection: Array<SkSection | SkItem>;
   docName: string = '';
   constructor(private docService: DocumentService
   ) { }
@@ -29,24 +29,25 @@ export class TaskViewerComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges() {
+    console.log('Here');
     if (this.docJson) {
       this.docName = JSON.parse(this.docJson).data;
-      this.taskList  = this.docService.SKTasksList(this.docJson);
+      this.sectionList  = this.docService.SKTasksList(this.docJson);
       this.doSearch();
       this.currentTask = 0;
-      this.setViewTask();
-      console.log(this.taskList);
+      this.setViewSection();
+      console.log(this.sectionList);
     }
   }
 
   doSearch(){
 
     if (this.isSearch && this.searchPhrase !== '') {
-      this.searchTaskArray = [];
-      this.currentSearchTask = 0;
+      this.searchSectionArray = [];
+      this.currentSearchSectionIndex = 0;
 
       let arrCounter = 0;
-      this.taskList.forEach( (item: any) => {
+      this.sectionList.forEach( (item: any) => {
           let isFound = false;
         const ref: string = `(\>[^\>\<]*)${this.searchPhrase}([^\>\<]*\<)`;
           if (item.docs[0].data.match(new RegExp(ref, 'g'))) { isFound = true; }
@@ -56,52 +57,60 @@ export class TaskViewerComponent implements OnInit, OnChanges {
             parent.data = parent.data.replace(new RegExp(ref, 'g'), `$1<span style="background-color: lightcoral;">${this.searchPhrase}</span>$2`);
           });
           if (isFound) {
-            this.searchTaskArray.push(arrCounter);
+            this.searchSectionArray.push(arrCounter);
           }
           arrCounter++;
         }
       );
-      console.log(this.searchTaskArray.length);
+      console.log(this.searchSectionArray);
     }
   }
 
-  nextTask() {
-    if (this.currentTask < this.taskList.length - 1) {
+  nextSection() {
+    if (this.currentTask < this.sectionList.length - 1) {
       this.currentTask++;
-      this.setViewTask();
+      this.setViewSection();
 
     }
   }
 
-  previousTask() {
+  previousSection() {
     if (this.currentTask > 0) {
       this.currentTask--;
-      this.setViewTask();
+      this.setViewSection();
     }
   }
 
-  nextSearchTask() {
-    if (this.currentSearchTask < this.searchTaskArray.length - 1) {
-      this.currentSearchTask++;
-      this.currentTask = this.searchTaskArray[this.currentSearchTask];
-      this.setViewTask();
-
+  nextSearchSection() {
+    this.currentSearchSectionIndex = this.searchSectionArray.length - 1;
+    for (let i = 0; i < this.searchSectionArray.length - 1; i++) {
+      if (this.searchSectionArray[i] > this.currentTask) {
+        this.currentSearchSectionIndex = i;
+        break;
+      }
     }
+    this.currentTask = this.searchSectionArray[this.currentSearchSectionIndex];
+    this.setViewSection();
   }
 
-  previousSearchTask() {
-    if (this.currentSearchTask > 0) {
-      this.currentSearchTask--;
-      this.currentTask = this.searchTaskArray[this.currentSearchTask];
-      this.setViewTask();
+  previousSearchSection() {
+    this.currentSearchSectionIndex = 0;
+    for (let i = this.searchSectionArray.length - 1; i > 0 ; i--) {
+      console.log(i, this.searchSectionArray[i], this.currentTask )
+      if (this.searchSectionArray[i] < this.currentTask) {
+        this.currentSearchSectionIndex = i;
+        break;
+      }
     }
+    this.currentTask = this.searchSectionArray[this.currentSearchSectionIndex];
+    this.setViewSection();
   }
 
-  setViewTask(){
-    const task: any = this.taskList[this.currentTask];
-    this.viewTask = [];
-    this.viewTask = [...task.parents, ...task.docs];
-    console.log(this.viewTask);
+  setViewSection(){
+    const task: any = this.sectionList[this.currentTask];
+    this.viewSection = [];
+    this.viewSection = [...task.parents, ...task.docs];
+    console.log(this.viewSection);
   }
 
   toggleSearch() {
